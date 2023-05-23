c8e795846809f7ff887e69e26d68e87374bf806e
FilipchenkoDima <blewdinor@gmail.com>
2023-05-23 22:07:11 +0300

    Create project, add register and login

---
+++ rn-project/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LoginScreen from "./Screens/LoginScreen/LoginScreen";
+import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
+
+export default function App() {
+  return <RegistrationScreen />;
+}
